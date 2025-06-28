4e9bd4c94847cd0ce3ddbaf55d07f8ceaeee7bac
Bharathi Institute <bharathiinstitute1@gmail.com>
2025-06-28 14:29:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 //potti
 //kanna
-
+//pooj
+//cat
+//jyo
 void main() {
   runApp(const MainApp());
 }
